46439ce84799050de6c7122018ce858ed4ab0296
saif1642 <islumsaiful@gmail.com>
2017-11-16 13:58:46 +0600

    First commit/Start the project through laravel project

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -9,8 +9,8 @@ window._ = require('lodash');
 
 try {
     window.$ = window.jQuery = require('jquery');
-
-    require('bootstrap-sass');
+    //we have vue.js
+    //require('bootstrap-sass');
 } catch (e) {}
 
 /**
